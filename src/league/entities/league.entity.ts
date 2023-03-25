8808ede5fd0eca7bbcbaa06818dcf12b9795b8c6
Rene Torres <rene_edgardo_2@hotmail.com>
2023-03-24 20:06:56 -0600

    Set league entity

--- src/league/entities/league.entity.ts
+++ src/league/entities/league.entity.ts
@@ -1,1 +1,10 @@
-export class League {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class League {
+  @PrimaryGeneratedColumn({ type: 'tinyint'})
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+}
