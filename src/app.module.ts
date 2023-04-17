3412b3b469e8ad4947c69b1f238ed7a4e25b2dfe
Rene Torres <rene_edgardo_2@hotmail.com>
2023-04-16 19:21:32 -0600

    Add dataSource to the export AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JerseyModule } from './jersey/jersey.module';
@@ -13,27 +14,29 @@ import { LeagueModule } from './league/league.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as any,
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE,
-      autoLoadEntities: true,
-      synchronize: JSON.parse(process.env.DATABASE_SYNCHRONIZE),
-      extra : {
-        connectionLimit: parseInt(process.env.DATABASE_CONNECTION_LIMIT)
-      }
-    }),
+  TypeOrmModule.forRoot({
+    type: process.env.DATABASE_TYPE as any,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    autoLoadEntities: true,
+    synchronize: JSON.parse(process.env.DATABASE_SYNCHRONIZE),
+    extra: {
+      connectionLimit: parseInt(process.env.DATABASE_CONNECTION_LIMIT)
+    }
+  }),
     VersionModule,
     JerseyModule,
     TeamModule,
     KitModule,
     SizeModule,
     PatchModule,
-    LeagueModule,],
+    LeagueModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
