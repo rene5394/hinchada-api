40f0815c775e31f14f08ab0a47a3f4fb187ce445
Rene Torres <rene_edgardo_2@hotmail.com>
2023-04-16 19:24:25 -0600

    Add migrations

---
+++ src/migrations/1681690282716-sizeDataSeeding.ts
@@ -0,0 +1,32 @@
+import { Size } from 'src/size/entities/size.entity';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class sizeDataSeeding1681690282716 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into(Size)
+      .values([
+        { name: 'S' },
+        { name: 'M' },
+        { name: 'L' },
+        { name: 'XL' },
+        { name: '2XL' },
+        { name: '3XL' },
+        { name: '4XL' }
+      ])
+      .execute();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .delete()
+      .from(Size)
+      .whereInIds([1, 2, 3, 4, 5, 6, 7])
+      .execute();
+  }
+
+}
---
+++ src/migrations/1681690294385-versionDataSeeding.ts
@@ -0,0 +1,27 @@
+import { Version } from 'src/version/entities/version.entity';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class versionDataSeeding1681690294385 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into(Version)
+      .values([
+        { name: 'Fan' },
+        { name: 'Player' }
+      ])
+      .execute();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .delete()
+      .from(Version)
+      .whereInIds([1, 2])
+      .execute();
+  }
+
+}
