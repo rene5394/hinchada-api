2bf7cd19fd7fa9b88285306a835456282f4ea582
Rene Torres <rene_edgardo_2@hotmail.com>
2023-04-23 02:35:42 -0600

    Add patch relationship for all jersey services

--- src/api/jersey/jersey.service.ts
+++ src/api/jersey/jersey.service.ts
@@ -1,31 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CreateJerseyDto } from './dto/create-jersey.dto';
 import { UpdateJerseyDto } from './dto/update-jersey.dto';
 import { Jersey } from './entities/jersey.entity';
+import { Patch } from '../patch/entities/patch.entity';
 
 @Injectable()
 export class JerseyService {
   constructor(
     @InjectRepository(Jersey)
-    private jerseyRepository: Repository<Jersey>
+    private jerseyRepository: Repository<Jersey>,
+    @InjectRepository(Patch)
+    private readonly patchRepository: Repository<Patch>
   ) {}
 
   async create(createJerseyDto: CreateJerseyDto) {
-    return await this.jerseyRepository.save(createJerseyDto);
+    const patches = await this.patchRepository.findBy({ id: In(createJerseyDto.patches) });
+    const jersey = this.jerseyRepository.create({ ...createJerseyDto, patches });
+
+    return await this.jerseyRepository.save(jersey);
   }
 
   async findAll(): Promise<Jersey[]> {
-    return await this.jerseyRepository.find();
+    return await this.jerseyRepository.find({
+      relations: {
+        patches: true
+      }
+    });
   }
 
   async findOne(id: number): Promise<Jersey> {
-    return await this.jerseyRepository.findOneBy({ id });
+    return await this.jerseyRepository.findOne({
+      where: { id },
+      relations: {
+        patches: true
+      }
+    });
   }
 
   async update(id: number, updateJerseyDto: UpdateJerseyDto) {
-    return await this.jerseyRepository.update(id, updateJerseyDto);
+    const patches = await this.patchRepository.findBy({ id: In(updateJerseyDto.patches) });
+    const oldJersey = await this.findOne(id);
+
+    const jersey = this.jerseyRepository.create({
+      ...oldJersey,
+      ...updateJerseyDto,
+      patches,
+    });
+
+    return await this.jerseyRepository.save(jersey);
   }
 
   async remove(id: number) {
