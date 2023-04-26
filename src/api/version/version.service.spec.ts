32b60459893735ddd6c0abb036477f6049930de5
Rene Torres <rene_edgardo_2@hotmail.com>
2023-04-25 20:45:55 -0600

    Add test for findAll and find version service

--- src/api/version/version.service.spec.ts
+++ src/api/version/version.service.spec.ts
@@ -1,18 +1,50 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { VersionService } from './version.service';
+import { Version } from './entities/version.entity';
 
 describe('VersionService', () => {
-  let service: VersionService;
+  let versionService: VersionService;
+  let versionRepository;
+
+  const mockVersionRepository = {
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [VersionService],
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        VersionService,
+        {
+          provide: getRepositoryToken(Version),
+          useValue: mockVersionRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<VersionService>(VersionService);
+    versionService = moduleRef.get<VersionService>(VersionService);
+    versionRepository = moduleRef.get(getRepositoryToken(Version));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of versions', async () => {
+      const mockVersions = [{ id: 1, name: 'Version 1' }];
+      jest.spyOn(versionRepository, 'find').mockResolvedValueOnce(mockVersions);
+      const versions = await versionService.findAll();
+      expect(versions).toEqual(mockVersions);
+      expect(versionRepository.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a version', async () => {
+      const mockVersion = { id: 1, name: 'Version 1' };
+      jest.spyOn(versionRepository, 'findOneBy').mockResolvedValueOnce(mockVersion);
+      const version = await versionService.findOne(1);
+      expect(version).toEqual(mockVersion);
+      expect(versionRepository.findOneBy).toHaveBeenCalledTimes(1);
+      expect(versionRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    });
   });
 });
